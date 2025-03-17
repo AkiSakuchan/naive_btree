e6cd783ba8bb333080bebff6cacaa5e888f068a3
AkiSakuchan <rou.leon@icloud.com>
2025-03-17 04:58:11 +0100

    更新继承测试的组织方式, 删除操作初步.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-use std::marker::PhantomData;
 use std::mem::replace;
 use std::ptr;
 
@@ -7,8 +6,7 @@ struct Node<K:Ord, V>
 {
     members: Vec<(K,V)>,
     children: Option<Vec<Box<Self>>>,
-    parent: Option<*mut Self>,
-    parent_idx: usize
+    parent: Option<(*mut Self, usize)>,
 }
 enum SearchResult<K:Ord, V>
 {
@@ -39,37 +37,33 @@ impl<K:Ord, V> Node<K,V>
         }
     }
 
-    /// 传入一个插入目标节点的指针, 如果不产生新的根节点则返回 None, 如果有新的跟节点, 则返回新根节点的 Box 指针.
-    unsafe fn insert(this: *mut Self, index: usize, key: K, value: V) -> Option<Box<Self>>
+    /// 传入一个插入目标节点的引用, 如果不产生新的根节点则返回 None, 如果有新的跟节点, 则返回新根节点的 Box 指针.
+    fn insert(this: &mut Self, index: usize, key: K, value: V) -> Option<Box<Self>>
     {
-        let this_ref = unsafe { this.as_mut().expect("核心 insert 不能传入空指针") };
-        this_ref.members.insert(index, (key,value));
+        this.members.insert(index, (key,value));
 
-        if this_ref.members.len() < RANK { None }
+        if this.members.len() < RANK { None }
         else {
-            let right_members = this_ref.members.split_off((RANK + 1) / 2);
-            let mid_member = this_ref.members.pop().unwrap();
+            let right_members = this.members.split_off((RANK + 1) / 2);
+            let mid_member = this.members.pop().unwrap();
 
             let mut new_right_node = Box::new(Self{
                 members: right_members,
                 children: None,
-                parent: None,
-                parent_idx: this_ref.parent_idx + 1
+                parent: None
             });
 
-            if let Some(ref mut children) = this_ref.children {
+            if let Some(ref mut children) = this.children {
                 let mut right_children = children.split_off((RANK + 1) / 2);
 
                 right_children.iter_mut().enumerate().for_each(|(i,child)| {
-                    child.parent_idx = i;
-                    
-                    child.parent = Some(box_as_mut_ptr(&mut new_right_node));
+                    child.parent = Some((box_as_mut_ptr(&mut new_right_node), i))
                 });
 
                 new_right_node.children = Some(right_children);
             }
 
-            match this_ref.parent
+            match this.parent
             {
                 None => {
                     let new_right_node_parent = ptr::from_mut(&mut new_right_node.parent); 
@@ -78,23 +72,24 @@ impl<K:Ord, V> Node<K,V>
 
                     let mut new_root_node = Box::new(Self{
                         members: vec![mid_member],
-                        children: Some(vec![ unsafe{ Box::from_raw(this) } , new_right_node]),
-                        parent: None,
-                        parent_idx: 0
+                        children: Some(vec![ unsafe{ Box::from_raw(ptr::from_mut(this)) }, new_right_node]),
+                        parent: None
                     });
-                    unsafe { *new_right_node_parent  = Some(box_as_mut_ptr(&mut new_root_node)); }
-                    this_ref.parent = Some(box_as_mut_ptr(&mut new_root_node));
+                    unsafe { *new_right_node_parent  = Some((box_as_mut_ptr(&mut new_root_node), 1)); }
+                    this.parent = Some((box_as_mut_ptr(&mut new_root_node), 0));
                     
                     Some(new_root_node)
                 }
-                Some(parent) => {
-                    new_right_node.parent = Some(parent);
+                Some((parent,parent_idx)) => {
+                    new_right_node.parent = Some((parent, parent_idx + 1));
 
                     unsafe{ 
-                        (*parent).children.as_mut().unwrap()[this_ref.parent_idx + 1 ..].iter_mut().for_each(|child| child.parent_idx += 1);
-                        (*parent).children.as_mut().unwrap().insert(new_right_node.parent_idx, new_right_node);
+                        (*parent).children.as_mut().unwrap()[parent_idx + 1 ..].iter_mut().for_each(|child|
+                            child.parent.as_mut().unwrap().1 += 1
+                        );
+                        (*parent).children.as_mut().unwrap().insert(parent_idx + 1, new_right_node);
 
-                        Self::insert(parent, this_ref.parent_idx, mid_member.0, mid_member.1)
+                        Self::insert(parent.as_mut().unwrap(), parent_idx, mid_member.0, mid_member.1)
                     }
                 }
             }
@@ -116,8 +111,7 @@ impl<K:Ord, V> Btree<K,V>
             root: Box::into_raw(Box::new(Node{
                 members: Vec::new(),
                 children: None,
-                parent: None,
-                parent_idx: 0,
+                parent: None
             }))
         }
     }
@@ -151,7 +145,7 @@ impl<K:Ord, V> Btree<K,V>
             {
                 SearchResult::Found(p, idx) => Some(replace(&mut (*p).members[idx].1, value)),
                 SearchResult::NonFound(p, idx) => {
-                    if let Some(new_root) = Node::insert(p, idx, key, value) {
+                    if let Some(new_root) = Node::insert(p.as_mut().unwrap(), idx, key, value) {
                         self.root = Box::into_raw(new_root);
                     }
                     None
@@ -170,10 +164,9 @@ impl<K:Ord, V> Drop for Btree<K,V>
 
 pub struct Iter<'a, K: Ord, V>
 {
-    pointer: *mut Node<K,V>,
+    current_node: &'a Node<K,V>,
     idx: usize,
-    is_first: bool,
-    _maker: PhantomData<&'a (K,V)>
+    is_first: bool
 }
 
 impl<K:Ord, V> Node<K,V> {
@@ -186,7 +179,7 @@ impl<K:Ord, V> Node<K,V> {
             else {
                 match unsafe {(*this).parent } {
                     None => None, 
-                    Some(parent) => unsafe { Self::get_next( parent, (*this).parent_idx, true) }
+                    Some((parent, index)) => unsafe { Self::get_next( parent, index, true) }
                 }
             }
         }
@@ -217,19 +210,17 @@ impl<'a, K:Ord, V> Iterator for Iter<'a, K,V>
     {
         if self.is_first {
             self.is_first = false;
-            unsafe {
-                if (*self.pointer).members.is_empty() { None }
-                else { Some( &(*self.pointer).members[self.idx] ) }
-            }
+            if self.current_node.members.is_empty() { None }
+            else { Some( &self.current_node.members[self.idx] ) }
         }
         else {
-            match unsafe { Node::get_next(self.pointer, self.idx, false) }
+            match unsafe { Node::get_next(ptr::from_ref(self.current_node) as *mut Node<K,V>, self.idx, false) }
             {
                 None => None,
                 Some((pt,i)) => {
-                    self.pointer = pt;
+                    self.current_node = unsafe { pt.as_ref().unwrap() };
                     self.idx = i;
-                    Some( unsafe { &(*self.pointer).members[i] })
+                    Some( &self.current_node.members[i] )
                 }
             }
         }
@@ -241,15 +232,62 @@ impl<'a, K:Ord, V> Btree<K,V> {
     {
         match unsafe{ &mut (*self.root).children }
         {
-            None => Iter{ pointer: self.root, idx:0,  is_first: true, _maker:PhantomData },
+            None => Iter{ current_node: unsafe { self.root.as_ref().unwrap() }, idx:0,  is_first: true },
             Some(children) => {
                 let mut ptr = &mut children[0];
                 while let Some(ref mut child) = (*ptr).children {
                     ptr = &mut child[0];
                 }
 
-                Iter{ pointer: box_as_mut_ptr(ptr), idx: 0, is_first: true, _maker:PhantomData }
+                Iter{ current_node: (*ptr).as_ref() , idx: 0, is_first: true }
             }
         }
+    }
+}
+
+
+impl<K: Ord, V> Node<K,V>
+{
+    /// 从兄弟节点移动成员到本节点, origin 是 true 表示右边节点减少成员, origin 是 false 表示左边节点减少成员. 本函数不检查左边或者右边是否有兄弟节点.
+    fn get_from_sibling(&mut self, origin: bool)
+    {
+        let (parent, parent_idx)  = unsafe { 
+            let (a,b) = self.parent.expect("必须要有父节点");
+            (a.as_mut().unwrap(), b)
+        };
+        if origin {
+            let right_sibling = &mut parent.children.as_mut().unwrap()[parent_idx + 1];
+            let new_mid_member = right_sibling.members.remove(0); //提取右兄弟的第一个成员
+            let new_member = replace(&mut parent.members[parent_idx], new_mid_member);
+            self.members.push(new_member);
+
+            if let Some(ref mut right_children) = right_sibling.children {
+                let mut child = right_children.remove(0);
+                right_children.iter_mut().for_each(|item| item.parent.as_mut().unwrap().1 -= 1);
+
+                child.parent = Some((ptr::from_mut(self), self.members.len()));
+                self.children.as_mut().unwrap().push(child);
+            }
+        }
+        else {
+            let left_sibling = &mut parent.children.as_mut().unwrap()[parent_idx - 1];
+            let new_mid_member = left_sibling.members.pop().unwrap();
+            let new_member = replace(&mut parent.members[parent_idx -1], new_mid_member);
+            self.members.insert(0, new_member);
+
+            if let Some(ref mut left_children) = left_sibling.children {
+                let mut child = left_children.pop().unwrap();
+                child.parent = Some((ptr::from_mut(self), 0));
+
+                self.children.as_mut().unwrap().iter_mut().for_each(|item| item.parent.as_mut().unwrap().1 += 1);
+                self.children.as_mut().unwrap().push(child);
+            }
+        }
+    }
+
+    /// 合并同级两个兄弟节点, 把第二个参数指向的节点和两节点之间的成员合并到第一个参数指向的节点, 并返回第一个节点.
+    fn merge(left_node: &mut Box<Self>, mid_member:(K,V), right_node: Box<Self>)
+    {
+        todo!()
     }
 }
--- tests/integrated_test.rs
+++ tests/integrated_test.rs
@@ -1,34 +1,36 @@
 use naive_btree::*;
 
+const DATA :[(i32,i32); 24] = [(1, 8),
+(4, 9),
+(6, 2),
+(8, 10),
+(11, 11),
+(13, 3),
+(14, 12),
+(16, 13),
+(17, 1),
+(19, 14),
+(22, 15),
+(23, 4),
+(27, 16),
+(34, 17),
+(35, 5),
+(38, 18),
+(45, 19),
+(47, 6),
+(49, 20),
+(53, 21),
+(55, 7),
+(65, 22),
+(74, 23),
+(79, 24)];
+
 fn init_test() -> Btree<i32, i32>
 {
 
 let mut btree = Btree::new();
 
-btree.insert(17, 1);
-btree.insert(6, 2);
-btree.insert(13, 3);
-btree.insert(23, 4);
-btree.insert(35, 5);
-btree.insert(47, 6);
-btree.insert(55, 7);
-btree.insert(1, 8);
-btree.insert(4, 9);
-btree.insert(8, 10);
-btree.insert(11, 11);
-btree.insert(14, 12);
-btree.insert(16, 13);
-btree.insert(19, 14);
-btree.insert(22, 15);
-btree.insert(27, 16);
-btree.insert(34, 17);
-btree.insert(38, 18);
-btree.insert(45, 19);
-btree.insert(49, 20);
-btree.insert(53, 21);
-btree.insert(65, 22);
-btree.insert(74, 23);
-btree.insert(79, 24);
+DATA.iter().for_each(|(a,b)| {btree.insert(*a, *b);} );
 
 btree
 }
@@ -38,30 +40,7 @@ fn read_works()
 {
 let btree_example = init_test();
 
-assert_eq!(*btree_example.get(&17).unwrap(), 1);
-assert_eq!(*btree_example.get(&6).unwrap(), 2);
-assert_eq!(*btree_example.get(&13).unwrap(), 3);
-assert_eq!(*btree_example.get(&23).unwrap(), 4);
-assert_eq!(*btree_example.get(&35).unwrap(), 5);
-assert_eq!(*btree_example.get(&47).unwrap(), 6);
-assert_eq!(*btree_example.get(&55).unwrap(), 7);
-assert_eq!(*btree_example.get(&1).unwrap(), 8);
-assert_eq!(*btree_example.get(&4).unwrap(), 9);
-assert_eq!(*btree_example.get(&8).unwrap(), 10);
-assert_eq!(*btree_example.get(&11).unwrap(), 11);
-assert_eq!(*btree_example.get(&14).unwrap(), 12);
-assert_eq!(*btree_example.get(&16).unwrap(), 13);
-assert_eq!(*btree_example.get(&19).unwrap(), 14);
-assert_eq!(*btree_example.get(&22).unwrap(), 15);
-assert_eq!(*btree_example.get(&27).unwrap(), 16);
-assert_eq!(*btree_example.get(&34).unwrap(), 17);
-assert_eq!(*btree_example.get(&38).unwrap(), 18);
-assert_eq!(*btree_example.get(&45).unwrap(), 19);
-assert_eq!(*btree_example.get(&49).unwrap(), 20);
-assert_eq!(*btree_example.get(&53).unwrap(), 21);
-assert_eq!(*btree_example.get(&65).unwrap(), 22);
-assert_eq!(*btree_example.get(&74).unwrap(), 23);
-assert_eq!(*btree_example.get(&79).unwrap(), 24);
+DATA.iter().for_each(|(a,b)| assert_eq!(btree_example.get(a).unwrap(), b));
 
 assert!(btree_example.get(&5).is_none(), "找到不存在的值");
 
@@ -89,6 +68,6 @@ fn iter_works()
 
     let btree = init_test();
     for it in btree.iter() {
-        println!("({}, {})", it.0, it.1);
+        println!("({}, {}),", it.0, it.1);
     }
 }
